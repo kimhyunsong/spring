b70958d3f4d56520aa265936b7b1056507c3409d
kimhyunsong <ssong_14@naver.com>
2022-10-19 21:58:02 +0900

    Spring

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,4 +16,9 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
